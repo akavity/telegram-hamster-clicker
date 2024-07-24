11c3092a1be093565929a6417ab0ef79134a6328
Basil Akavity <vakavity@gmail.com>
2024-07-24 21:51:54 +0300

    added Utils

---
+++ src/main/java/org/akavity/utils/Utils.java
@@ -0,0 +1,43 @@
+package org.akavity.utils;
+
+import com.codeborne.selenide.SelenideElement;
+import com.codeborne.selenide.appium.AppiumDriverRunner;
+import io.appium.java_client.TouchAction;
+import io.appium.java_client.touch.WaitOptions;
+import io.appium.java_client.touch.offset.PointOption;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
+
+import static com.codeborne.selenide.WebDriverRunner.getWebDriver;
+
+public class Utils {
+    public static void sleep(long millis) {
+        try {
+            Thread.sleep(millis);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public static void sleep() {
+        try {
+            Thread.sleep(1000);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public static void waitUntilClickable(SelenideElement element) {
+        WebDriverWait wait = new WebDriverWait(getWebDriver(), Duration.ofMillis(30000));
+        wait.until(ExpectedConditions.elementToBeClickable(element));
+    }
+
+    public static void clickCoordinates(int x, int y) {
+        TouchAction touch = new TouchAction(AppiumDriverRunner.getAndroidDriver());
+        touch.press(PointOption.point(x, y))
+                .waitAction(WaitOptions.waitOptions(Duration.ofMillis(200)))
+                .release().perform();
+    }
+}
