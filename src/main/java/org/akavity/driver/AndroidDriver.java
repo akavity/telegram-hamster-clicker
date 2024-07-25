adc3524a02293840cd01e99f4e63b82c2260d2df
Basil Akavity <vakavity@gmail.com>
2024-07-25 23:56:02 +0300

    updated AndroidDriver

--- src/main/java/org/akavity/driver/AndroidDriver.java
+++ src/main/java/org/akavity/driver/AndroidDriver.java
@@ -11,6 +11,8 @@ import javax.annotation.ParametersAreNonnullByDefault;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.time.Duration;
+import java.util.HashMap;
+import java.util.Map;
 
 @ParametersAreNonnullByDefault
 public class AndroidDriver implements WebDriverProvider {
@@ -19,11 +21,17 @@ public class AndroidDriver implements WebDriverProvider {
     @CheckReturnValue
     @Nonnull
     public WebDriver createDriver(Capabilities capabilities) {
+        Map<String, Object> map = new HashMap<>();
+        map.put("ignoreUnimportantViews", true);
+        map.put("allowInvisibleElements", true);
+
         UiAutomator2Options options = new UiAutomator2Options();
+
         options.merge(capabilities);
+        options.setCapability("appium:settings", map);
         options.setPlatformName("Android");
         options.setPlatformVersion("14.0");
-        options.setDeviceName("Medium Phone");
+        options.setDeviceName("Medium Tablet");
         options.setNewCommandTimeout(Duration.ofSeconds(11));
         options.setFullReset(false);
         options.setApp("D:/stady/QA automation engineer/IT-Academy/class 15/telegram.apk");
