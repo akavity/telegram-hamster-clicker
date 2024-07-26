fae995621b8ffeb952c370c49e71f2aceb445078
Basil Akavity <vakavity@gmail.com>
2024-07-26 18:14:02 +0300

    updated utils

--- src/main/java/org/akavity/utils/Utils.java
+++ src/main/java/org/akavity/utils/Utils.java
@@ -3,12 +3,13 @@ package org.akavity.utils;
 import com.codeborne.selenide.SelenideElement;
 import com.codeborne.selenide.appium.AppiumDriverRunner;
 import io.appium.java_client.TouchAction;
-import io.appium.java_client.touch.WaitOptions;
 import io.appium.java_client.touch.offset.PointOption;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.time.Duration;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 import static com.codeborne.selenide.WebDriverRunner.getWebDriver;
 
@@ -37,5 +38,16 @@ public class Utils {
     public static void clickCoordinates(int x, int y) {
         TouchAction touch = new TouchAction(AppiumDriverRunner.getAndroidDriver());
         touch.press(PointOption.point(x, y)).release().perform();
+    }
+
+    public static int extractIntegerFromText(String text, String regex) {
+        int result = 0;
+        Pattern pattern = Pattern.compile(regex);
+        Matcher matcher = pattern.matcher(text);
+        while (matcher.find()) {
+            result = Integer.parseInt(matcher.group().replace(" /", "")
+            );
+        }
+        return result;
     }
 }
